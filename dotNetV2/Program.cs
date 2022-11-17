c54a699c707a793ec00b2081cedbb8d38aa8f8ea
SolomonDa <solomonda@stlghana.com>
2022-11-17 08:33:02 +0000

    Done

--- dotNetV2/Program.cs
+++ dotNetV2/Program.cs
@@ -4,9 +4,35 @@ namespace dotNetV2
 {
     class Program
     {
+        public struct Book
+        {
+            public decimal price;
+            public string author;
+            public string description;
+
+
+        }
+
         static void Main(string[] args)
         {
-            
+            int[] myArr = new int[6];
+
+            myArr[0] = 34;
+            myArr[1] = 4;
+            myArr[2] = 14;
+            myArr[3] = 24;
+            myArr[4] = 44;
+            myArr[5] = 54;
+
+            Console.WriteLine(myArr.Length);
+
+            Book book;
+
+            book.price = 12.12m;
+            book.author = "Pythagoras";
+            book.description = "Book for Sanjay";
+
+
         }
     }
 }
